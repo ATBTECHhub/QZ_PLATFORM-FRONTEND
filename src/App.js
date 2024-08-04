327b78e69aab18cd50993a70d5897328a9a55cba
Adebiyi Ridwan <donrealex11@gmail.com>
2024-08-04 20:47:20 +0100

    Qz Platform

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Routes } from "react-router-dom";
+
+function App() {
+ return (
+    <div>
+      <Routes>
+
+      </Routes>
+    </div>
+}
+  <Routes>
+ Rou
+
+    </Routes>
+
+export default App;
+ 
